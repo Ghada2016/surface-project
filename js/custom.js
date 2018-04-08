345723869f932ca8e2cbd4c206f0952d6ce3bd89
Ghada Gamal <gh_mostafa@hotmail.com>
2018-04-08 13:57:15 +0200

    Last modification

--- js/custom.js
+++ js/custom.js
@@ -1,49 +1,7 @@
-// resposive top navbar
-function myFunction(){
-    $('.tog').click(function(){
-        $('.navbar').toggle();
-    });  
-}
-
-//Initialize the slider
-    
-    $(document).ready(function () {
-        $('.bxslider').bxSlider(); 
+   // Trigger nice scroll
+    $('html').niceScroll({
+        cursorcolor: '#5e42b0',
+        cursorwidth: '10px',
+        cursorborder: '0 ',
+		
     });
-// add and remove class (active)
-    $('li a').click(function () {
-        $('li a.active').removeClass('active');
-        $(this).closest('a').addClass('active');
-    });
-// scroll
-$(document).scroll(function(){
-	$('nav').css('background-color','#333');
-});
-
-// Html animate
-    $('nav li a').click(function () {
-        $('html, body').animate({
-            scrollTop: $('#' + $(this).data('value')).offset().top
-        }, 2000);
-        console.log($(this).data('value'));
-    });
-
-// up button
-     // When the user scrolls down 20px from the top of the document, show the button
-    var myBtn = document.getElementById('btn');
-    
-    window.onscroll = function scrollFunction() {
-        
-        if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-            myBtn.style.display = "block";
-        } else {
-            myBtn.style.display = "none";
-        }
-    };
-    // When the user clicks on the button, scroll to the top of the document
-    myBtn.onclick = function topFunction() {
-        document.body.scrollTop = 0;
-        document.documentElement.scrollTop = 0;
-        $('li a').removeClass('active');
-        $('li a').first().addClass('active');
-    };
